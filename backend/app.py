72b6dd22c6d7c516e48dc29a070b03bcf063a64b
basil-bassey <akpanbasilbassey@gmail.com>
2026-02-03 21:00:03 +0100

    Phase 3: Added fake database and /student/<id> API endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+#Author: Basil Bassey
 
-@app.route("/")
-def home():
-    return "Cache System Backend Running!"
+from flask import Flask, jsonify
+from flask_cors import CORS
+import database  # import your fake database
+import time
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/student/<int:student_id>")
+def get_student(student_id):
+    start = time.time()  # start timer
+
+    student = database.get_student(student_id)
+
+    if not student:
+        return jsonify({"error": "Student not found"}), 404
+
+    end = time.time()  # end timer
+    response_time = round(end - start, 3)
+
+    return jsonify({
+        "data": student,
+        "source": "database",
+        "time": response_time
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
